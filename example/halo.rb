a3d1f06148ea84c502a1cb0fd09965504ec20151
TOKI Yoshinori <toki@freedom.ne.jp>
2019-01-13 22:09:23 +0900

    halo example: error capture

--- example/halo.rb
+++ example/halo.rb
@@ -33,56 +33,60 @@ stdout_log = Logger.new(STDOUT)
 protocol_log = Logger.new(File.join(File.dirname(__FILE__), 'protocol.log'))
 
 server.dispatch{|socket|
-  read_poll = Riser::ReadPoll.new(socket)
-  stream = Riser::WriteBufferStream.new(socket)
-  stream = Riser::LoggingStream.new(stream, protocol_log)
+  begin
+    read_poll = Riser::ReadPoll.new(socket)
+    stream = Riser::WriteBufferStream.new(socket)
+    stream = Riser::LoggingStream.new(stream, protocol_log)
 
-  catch(:end_of_connection) {
-    count = 0
-    while (count < 100)
-      count += 1
+    catch(:end_of_connection) {
+      count = 0
+      while (count < 100)
+        count += 1
 
-      until (read_poll.call(1))
-        if (read_poll.interval_seconds >= 10) then
-          throw(:end_of_connection)
-        end
-      end
-
-      begin
-        if (request_line = stream.gets) then
-          if (request_line =~ %r"\A (\S+) \s (\S+) \s (HTTP/\S+) \r\n \z"x) then
-            method, path, version = $1, $2, $3
-            while (line = stream.gets)
-              if (line == "\r\n") then
-                break
-              end
-            end
-            stdout_log.info("#{method} #{path} #{version}")
-
-            t = Time.now
-            case (method)
-            when 'GET'
-              stream << "HTTP/1.0 200 OK\r\n"
-              stream << "Content-Type: text/html\r\n"
-              stream << "Content-Length: #{HALO.bytesize}\r\n"
-              stream << "Date: #{t.httpdate}\r\n"
-              stream << "\r\n"
-              stream << HALO
-            else
-              stream << "HTTP/1.0 405 Method Not Allowed\r\n"
-              stream << "Content-Type: text/plain\r\n"
-              stream << "Date: #{t.httpdate}\r\n"
-              stream << "\r\n"
-              stream << "#{method} is not allowed.\r\n"
-              throw(:end_of_connection)
-            end
+        until (read_poll.call(1))
+          if (read_poll.interval_seconds >= 10) then
+            throw(:end_of_connection)
           end
         end
-      ensure
-        stream.flush
+
+        begin
+          if (request_line = stream.gets) then
+            if (request_line =~ %r"\A (\S+) \s (\S+) \s (HTTP/\S+) \r\n \z"x) then
+              method, path, version = $1, $2, $3
+              while (line = stream.gets)
+                if (line == "\r\n") then
+                  break
+                end
+              end
+              stdout_log.info("#{method} #{path} #{version}")
+
+              t = Time.now
+              case (method)
+              when 'GET'
+                stream << "HTTP/1.0 200 OK\r\n"
+                stream << "Content-Type: text/html\r\n"
+                stream << "Content-Length: #{HALO.bytesize}\r\n"
+                stream << "Date: #{t.httpdate}\r\n"
+                stream << "\r\n"
+                stream << HALO
+              else
+                stream << "HTTP/1.0 405 Method Not Allowed\r\n"
+                stream << "Content-Type: text/plain\r\n"
+                stream << "Date: #{t.httpdate}\r\n"
+                stream << "\r\n"
+                stream << "#{method} is not allowed.\r\n"
+                throw(:end_of_connection)
+              end
+            end
+          end
+        ensure
+          stream.flush
+        end
       end
-    end
-  }
+    }
+  rescue
+    stdout_log.error($!)
+  end
 }
 
 socket = TCPServer.new(host, port)
